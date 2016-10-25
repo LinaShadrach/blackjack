d0ddd951f993b5444efcc5e7150b34bdea25c9b4
Lina Shadrach and Matt Eichsteadt <matt@>
2016-10-25 16:38:39 -0700

    take two cards and tell user if they or the computer won

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,70 @@
-
+var compCards = [];
+var userCards = [];
+var userIndex = [];
+var compIndex = [];
 var ranks = ["ace","two","three","four","five","six","seven","eight","nine","ten","jack","queen","king"];
 var suits = ["hearts", "clubs", "spades", "diamonds"];
 var deck = [];
-
 suits.forEach(function(suit){
   ranks.forEach(function(rank){
-  deck.push(rank + " of " + suit);
+    deck.push(rank + " of " + suit);
+  });
+});
+var deck2 = deck.slice();
+for (var i = 0; i <2; i++){
+  var position = Math.round(Math.random() * (deck2.length-1));
+  userCards.push(deck2.splice(position, 1).toString());
+  position = Math.round(Math.random() * (deck2.length-1));
+  compCards.push(deck2.splice(position,1).toString());
+}
+
+deck.forEach(function(card){
+  userCards.forEach(function(userCard){
+    if (card === userCard){
+      userIndex.push(deck.indexOf(card));
+    }
+  });
+  compCards.forEach(function(compCard){
+    if (card === compCard){
+      compIndex.push(deck.indexOf(card));
+    }
   });
 });
 
-var position = Math.round(Math.random() * 51);
-var position2 = Math.round(Math.random() * 51);
-var position3 = Math.round(Math.random() * 51);
-var position4 = Math.round(Math.random() * 51);
-var positions = [position,position2, position3, position4];
+var sum = function(rank1, rank2){
+   return rank1 + rank2;
+};
 
-// if(position2 > position){
-//   position2 += 1;
-// }
-do {
-  for (var i = 0; i <4; i++){
-    for (var j = i+1; j<4; j++){
-      if (position[i] === position[j]){
-        position[j] = Math.round(Math.random() * 51);
-      }
-    }
+var toRank = function(array){
+  rank1 = ((array[0]) % 13) +1;
+  if (rank1>10){
+    rank1 = 10;
   }
-} while (position === position2 || position2 === position3 || position3 === position4 || position === position3 || position === position4 || position2 === position4 || position3 ===position4);
-
-var userCard = deck[position];
-var userCard2 = deck[position];
-
-var userCardRank = (position % 13) +1;
-var userCardRank2 = (position % 13) +1;
-
-var compCard = deck[position2];
-var compCard = deck[position2];
-
-var compCardRank = (position2 % 13) +1;
-var compCardRank = (position2 % 13) +1;
-
-
-var compCard = deck[position2];
-
+  rank2 = ((array[1]) % 13) +1;
+  if (rank2>10){
+    rank2 = 10;
+  }
+  var total = sum(rank1, rank2);
+  if(total > 12){
+    if(rank1 === 11){
+      rank1 = 1;
+    }
+    else if(rank2 === 11){
+      rank2 = 1;
+    }
+    total = sum(rank1, rank2);
+  }
+  if(total < 12){
+    if(rank1 === 1){
+      rank1 = 11;
+    }
+    else if(rank2 === 1){
+      rank2 = 11;
+    }
+    total = sum(rank1, rank2);
+  }
+  return total;
+};
 
 var compare = function(user, computer){
   if (user > computer) {
@@ -53,19 +75,27 @@ var compare = function(user, computer){
     return "tie";
   }
 };
+
+var userTotal = toRank(userIndex);
+var compTotal = toRank(compIndex);
+compare(userTotal, compTotal);
+
+
 $(document).ready(function(){
-
-
   $("button").click(function(event){
-    $(".userCard").text(userCard);
-    $(".compCard").text(compCard);
-    if(compare(userCardRank, compCardRank) === "userWins"){
-      $(".winner").text(userCard);
+    $(".userCard").text(userCards.join(', '));
+    $(".compCard").text(compCards.join(', '));
+    $(".userScore").text(userTotal);
+    $(".compScore").text(compTotal);
+    if(compare(userTotal, compTotal) === "userWins"){
+      $(".winner").text("You win!");
       $("#winner").show();
-    }  else if (compare(userCardRank, compCardRank) === "compWins") {
-      $(".winner").text(compCard);
+    }
+    else if (compare(userTotal, compTotal) === "compWins") {
+      $(".winner").text("Computer wins.");
       $("#winner").show();
-    }else {
+    }
+    else {
       $(".tie").text("it's a tie!");
 
     }
