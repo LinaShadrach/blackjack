e71d247a9bb6fbb07a752e53826e995d7168205c
Diego Suarez and Lina Shadrach <diego@>
2016-10-31 11:40:40 -0700

    create new branch. second try

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,3 @@
-var compCards = [];
-var userCards = [];
-var userIndex = [];
-var compIndex = [];
-var ranks = ["ace","two","three","four","five","six","seven","eight","nine","ten","jack","queen","king"];
-var suits = ["hearts", "clubs", "spades", "diamonds"];
-var deck = [];
-suits.forEach(function(suit){
-  ranks.forEach(function(rank){
-    deck.push(rank + " of " + suit);
-  });
-});
-var deck2 = deck.slice();
-for (var i = 0; i <2; i++){
-  var position = Math.round(Math.random() * (deck2.length-1));
-  userCards.push(deck2.splice(position, 1).toString());
-  position = Math.round(Math.random() * (deck2.length-1));
-  compCards.push(deck2.splice(position,1).toString());
-}
-
-deck.forEach(function(card){
-  userCards.forEach(function(userCard){
-    if (card === userCard){
-      userIndex.push(deck.indexOf(card));
-    }
-  });
-  compCards.forEach(function(compCard){
-    if (card === compCard){
-      compIndex.push(deck.indexOf(card));
-    }
-  });
-});
-
-var sum = function(rank1, rank2){
-   return rank1 + rank2;
-};
-
-var toRank = function(array){
-  rank1 = ((array[0]) % 13) +1;
-  if (rank1>10){
-    rank1 = 10;
-  }
-  rank2 = ((array[1]) % 13) +1;
-  if (rank2>10){
-    rank2 = 10;
-  }
-  var total = sum(rank1, rank2);
-  if(total > 12){
-    if(rank1 === 11){
-      rank1 = 1;
-    }
-    else if(rank2 === 11){
-      rank2 = 1;
-    }
-    total = sum(rank1, rank2);
-  }
-  if(total < 12){
-    if(rank1 === 1){
-      rank1 = 11;
-    }
-    else if(rank2 === 1){
-      rank2 = 11;
-    }
-    total = sum(rank1, rank2);
-  }
-  return total;
-};
-
 var compare = function(user, computer){
   if (user > computer) {
     return "userWins";
@@ -76,22 +8,30 @@ var compare = function(user, computer){
   }
 };
 
-var userTotal = toRank(userIndex);
-var compTotal = toRank(compIndex);
-compare(userTotal, compTotal);
+checkAce(userCards);
+checkAce(compCards);
+
+userScore = calculate(userCards);
+compScore = calculate(compCards);
+compare(userScore, compScore);
+
 
 
 $(document).ready(function(){
   $("button").click(function(event){
-    $(".userCard").text(userCards.join(', '));
-    $(".compCard").text(compCards.join(', '));
-    $(".userScore").text(userTotal);
-    $(".compScore").text(compTotal);
-    if(compare(userTotal, compTotal) === "userWins"){
+    var deck=buildDeck();
+
+    newGame= new Blackjack(deck,)
+
+    $(".userCard").text("The " + userCards[0].rank + " of " + userCards[0].suit + " and the " + userCards[1].rank + " of " + userCards[1].suit);
+    $(".compCard").text("The " + compCards[0].rank + " of " + compCards[0].suit + " and the " + compCards[1].rank + " of " + compCards[1].suit);
+    $(".userScore").text(userScore);
+    $(".compScore").text(compScore);
+    if(compare(userScore, compScore) === "userWins"){
       $(".winner").text("You win!");
       $("#winner").show();
     }
-    else if (compare(userTotal, compTotal) === "compWins") {
+    else if (compare(userScore, compScore) === "compWins") {
       $(".winner").text("Computer wins.");
       $("#winner").show();
     }
